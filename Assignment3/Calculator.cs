b48526842dfc35909d6067b8c41e6d6375efee29
Abhinav Agnihotri <Aagnihotri7347@conestogac.on.ca>
2021-07-24 20:44:09 +0530

    code before introducing mutant

---
+++ Assignment3/Calculator.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Assignment3
+{
+   public  class Calculator
+    {
+        public static double result;
+        public static double num1, num2;  
+
+        public static void Addition(double n1, double n2)
+        {
+            try
+            {
+                result = Math.Round((n1 + n2), 2, MidpointRounding.AwayFromZero);
+                Console.WriteLine("The Sum of {0} and {1} post rounding off 2-decimal digits(if exist) is = {2}", n1, n2, result);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+
+        public static void Subtraction(double n1, double n2)
+        {
+            try
+            {
+                result = Math.Round((n1 - n2), 2, MidpointRounding.AwayFromZero);
+                Console.WriteLine("After Subtracting {1} from {0} and the result after rounding off 2-decimal digits(if exist) is : {2}", n1, n2, result);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+
+        public static void Multiplication(double n1, double n2)
+        {
+            try
+            {
+                result = Math.Round((n1 * n2), 2, MidpointRounding.AwayFromZero);
+                Console.WriteLine("The Multiplication result of {0} and {1} post rounding off 2-decimal digits(if exist) is : {2}", n1, n2, result);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+
+        public static void Division(double n1, double n2)
+        {
+            try
+            {
+                if (n2 != 0)
+                {
+                    result = Math.Round((n1 / n2), 2, MidpointRounding.AwayFromZero);
+                    Console.WriteLine("The division result of {0} & {1} post rounding off 2-decimal digits(if exist) is : {2}", n1, n2, result);
+                }
+                else if (n2 == 0)
+                {
+                    result = (n1 / n2);
+                    if (result == double.PositiveInfinity)
+                    {
+                        Console.WriteLine("The result after dividing {0} by {1} is: {2} ", n1, n2, result);
+                        throw new DivideByZeroException("You can't divide any number by \"0\" !!! ");
+                    }
+                }
+                else
+                    Console.WriteLine("Wrong input, please try again later");
+            }
+
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+
+        }
+    }
+}
---
+++ Assignment3/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+
+namespace Assignment3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.WriteLine("::::::Awesome Calculator:::::");
+                Console.WriteLine("-----Menu options are as follows-------");
+                Console.WriteLine("1. Addition");
+                Console.WriteLine("2. Subtraction");
+                Console.WriteLine("3. Multiplication");
+                Console.WriteLine("4. Division");
+                Console.WriteLine("0. Exit");
+
+                Console.WriteLine("Hey User! Which operation you would like to perform?:");
+                int choice = Convert.ToInt32(Console.ReadLine());
+
+                if (choice == 1 || choice == 2 || choice == 3 || choice == 4)
+                {
+                    Console.WriteLine("Enter your 1st number here:");
+                    Calculator.num1 = Convert.ToDouble(Console.ReadLine());
+
+                    Console.WriteLine("Enter your 2nd number here:");
+                    Calculator.num2 = Convert.ToDouble(Console.ReadLine());
+                }
+
+                switch (choice)
+                {
+                    case 1:
+                        Calculator.Addition(Calculator.num1, Calculator.num2);
+                        break;
+
+                    case 2:
+                        Calculator.Subtraction(Calculator.num1, Calculator.num2);
+                        break;
+
+                    case 3:
+                        Calculator.Multiplication(Calculator.num1, Calculator.num2);
+                        break;
+
+                    case 4:
+                        Calculator.Division(Calculator.num1, Calculator.num2);
+                        break;
+
+                    case 0:
+                        Console.WriteLine("User has entered \"0\" for Exit. See you next time");
+                        break;
+
+                    default:
+                        Console.WriteLine("Sorry you have provided some incorrect input(s) !!");
+                        break;
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+
+
+        }
+    }
+}
